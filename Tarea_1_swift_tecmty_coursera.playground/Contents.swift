0a66b8eab6b3b7bdec7f44377c0e262c5cef901e
Armando Miranda <miranda.armando@outlook.com>
2015-11-15 18:55:16 -0600

    Se agrega el caracter '#' a cada uno de los mensajes que aparecerán en pantalla al ejecutar el playground.

--- Tarea_1_swift_tecmty_coursera.playground/Contents.swift
+++ Tarea_1_swift_tecmty_coursera.playground/Contents.swift
@@ -11,22 +11,22 @@ for numero in rango{
     
     if numero % 5 == 0{
         
-        print("\(numero) Bingo!!!" )
+        print("# \(numero) Bingo!!!" )
         
     }
     
     if numero % 2 == 0{
         
-        print("\(numero) par!!!" )
+        print("# \(numero) par!!!" )
         
     }else{
         
-        print("\(numero) impar!!!")
+        print("# \(numero) impar!!!")
     }
     
     if numero >= 30 && numero <= 40{
         
-        print("Viva Swift!!!")
+        print("# \(numero) Viva Swift!!!")
     }
     
     
